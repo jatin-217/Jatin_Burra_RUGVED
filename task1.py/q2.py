5f76fd2dafe0a239cb7b53a5274dd4c4b1fcdfad
jatin-217 <jatinramb@gmail.com>
2026-09-08 15:53:59 +0530

    Delete task1.py directory

--- task1.py/q2.py
+++
@@ -1,7 +0,0 @@
-s = input("Enter a string: ")
-s = s.replace(" ", "").lower()
-sorted_str=''.join(sorted(s))
-print("Sorted string:",sorted_str)
-for ch in sorted(set(sorted_str)):
-    print(f"{ch}:{sorted_str.count(ch)}")
-print("Total letters:",len(sorted_str))
--- task1.py/q5.py
+++
@@ -1,8 +0,0 @@
-def fibonacci(n):
-    if n==0:
-        return 0
-    elif n==1:
-        return 1
-    return fibonacci(n-1) + fibonacci(n-2)
-num = int(input("Enter a number: "))
-print(f"Fibonacci of {num} is {fibonacci(num)}")
